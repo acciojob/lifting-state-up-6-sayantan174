1cc77fd3a9684d3225f1f96b12ab97bbf6e03b83
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-23 17:59:42 +0530

    submitting code-Fri Jun 23 2023 17:59:41 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,8 @@ const App = () => {
         <h3>Child Component</h3>
         <ul>
           <li>Learn React {btn1 && <button onClick={()=>{setbtn1(!btn1)}}>Complete</button>}</li>
-          <li>Build a React app {btn2 && <button onClick={()=>{setbtn2(!btn2)}}>Complete</button>}</li>
-          <li>Deploy the React app {btn3 && <button onClick={()=>{setbtn3(!btn3)}}>Complete</button>}</li>
+          {/* <li>Build a React app {btn2 && <button onClick={()=>{setbtn2(!btn2)}}>Complete</button>}</li>
+          <li>Deploy the React app {btn3 && <button onClick={()=>{setbtn3(!btn3)}}>Complete</button>}</li> */}
         </ul>
     </div>
   )
