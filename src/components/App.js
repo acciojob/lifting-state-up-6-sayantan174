ad5451b808b4e5e72395d1c85baae771710e3671
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-23 14:45:23 +0530

    submitting code-Fri Jun 23 2023 14:45:22 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,13 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <h3>Child Component</h3>
+        <ul>
+          <li>Learn React <button>Complete</button></li>
+          <li>Build a React app <button>Complete</button></li>
+          <li>Deploy the React app <button>Complete</button></li>
+        </ul>
     </div>
   )
 }
