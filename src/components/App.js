a8f879c6e488d84b2436fb54b86c34b25b630cb2
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-23 14:57:43 +0530

    submitting code-Fri Jun 23 2023 14:57:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let btn1=true;
-  let btn2=true;
-  let btn3=true;
+  const [btn1,setbtn1]=useState(true);
+  const [btn2,setbtn2]=useState(true);
+  const [btn3,setbtn3]=useState(true);
+
+
   return (
     <div>
         {/* Do not remove the main div */}
         <h1>Parent Component</h1>
         <h3>Child Component</h3>
         <ul>
-          <li>Learn React {btn1 && <button onClick={()=>{btn1=!btn1}}>Complete</button>}</li>
-          <li>Build a React app {btn2 && <button onClick={()=>{btn2=!btn2}}>Complete</button>}</li>
-          <li>Deploy the React app {btn3 && <button onClick={()=>{btn3=!btn3}}>Complete</button>}</li>
+          <li>Learn React {btn1 && <button onClick={()=>{setbtn1(!btn1)}}>Complete</button>}</li>
+          <li>Build a React app {btn2 && <button onClick={()=>{setbtn2(!btn2)}}>Complete</button>}</li>
+          <li>Deploy the React app {btn3 && <button onClick={()=>{setbtn3(!btn3)}}>Complete</button>}</li>
         </ul>
     </div>
   )
