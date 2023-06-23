81168c4b9d42e85eb8572d50aefd4a6b9d2989c9
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-23 14:51:05 +0530

    submitting code-Fri Jun 23 2023 14:51:03 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,18 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let btn1=true;
+  let btn2=true;
+  let btn3=true;
   return (
     <div>
         {/* Do not remove the main div */}
         <h1>Parent Component</h1>
         <h3>Child Component</h3>
         <ul>
-          <li>Learn React <button>Complete</button></li>
-          <li>Build a React app <button>Complete</button></li>
-          <li>Deploy the React app <button>Complete</button></li>
+          <li>Learn React {btn1 && <button onClick={()=>{btn1=!btn1}}>Complete</button>}</li>
+          <li>Build a React app {btn2 && <button onClick={()=>{btn2=!btn2}}>Complete</button>}</li>
+          <li>Deploy the React app {btn3 && <button onClick={()=>{btn3=!btn3}}>Complete</button>}</li>
         </ul>
     </div>
   )
